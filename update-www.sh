49f41a5733d7e7be8b0d7e8b25609261d3e73aa1
Greg Ledbetter <gsledbetter@gmail.com>
2014-07-25 12:04:30 -0400

    Update condition-content-map.txt file as well as content HTML files.

--- update-www.sh
+++ update-www.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 cp ~/informaticslab/lydia-py/mc-gen/gen/conditions/*.html ~/informaticslab/lydia-droid/app/src/main/assets/content/
+cp ~/informaticslab/lydia-py/mc-gen/gen/condition-content-map.txt ~/informaticslab/lydia-droid/app/src/main/assets/content/
