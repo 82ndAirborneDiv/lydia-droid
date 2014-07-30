cb4bcff1f8c16779a63f83eafb05184e5fcc5793
Greg Ledbetter <gsledbetter@gmail.com>
2014-07-30 12:45:55 -0400

    Add method to reset current condtion reference.

--- app/src/main/java/gov/cdc/stdtxguide/ConditionContent.java
+++ app/src/main/java/gov/cdc/stdtxguide/ConditionContent.java
@@ -38,6 +38,10 @@ public class ConditionContent {
 
     }
 
+    public void resetCurrentCondition() {
+        currCondition = rootCondition;
+    }
+
     private InputStream readContentMapFromFile() {
 
         InputStream stream = null;
